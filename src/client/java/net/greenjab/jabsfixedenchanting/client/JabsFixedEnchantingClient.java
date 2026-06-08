6ad74d2dfd2a933b86b32c8586b0d002f6652342
joshua <joshua@baikie.org>
2026-06-08 19:12:15 +1000

    init
    init

---
+++ src/client/java/net/greenjab/jabsfixedenchanting/client/JabsFixedEnchantingClient.java
@@ -0,0 +1,10 @@
+package net.greenjab.jabsfixedenchanting.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JabsFixedEnchantingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
