9dce5e92a45a85f2f44fb1e35ff7119cf40fe984
joshua <joshua@baikie.org>
2026-06-17 15:32:20 +1000

    forgot fisherman enchanted books

--- src/main/java/net/greenjab/jabsfixedenchanting/mixin/MobMixin.java
+++ src/main/java/net/greenjab/jabsfixedenchanting/mixin/MobMixin.java
@@ -1,12 +1,17 @@
 package net.greenjab.jabsfixedenchanting.mixin;
 
 import net.greenjab.jabsfixedenchanting.enchanting.JabsFixedEnchantmentHelper;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.biome.Biomes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Mob.class)
 public abstract class MobMixin extends LivingEntity {
@@ -30,5 +35,16 @@ public abstract class MobMixin extends LivingEntity {
     private ItemStack applySuperEnchantArmor(
             ItemStack stack) {
         return JabsFixedEnchantmentHelper.applySuperEnchants(stack, random, this.level().getBiome(this.blockPosition()).is(Biomes.PALE_GARDEN));
+    }
+
+    @Inject(method = "finalizeSpawn", at=@At(value = "HEAD"))
+    private void addNightTag(ServerLevelAccessor level, DifficultyInstance difficulty, EntitySpawnReason spawnReason, SpawnGroupData groupData,
+                             CallbackInfoReturnable<SpawnGroupData> cir){
+        Mob LE = (Mob)(Object)this;
+        if (LE instanceof Monster HE) {
+            if (level.getBrightness(LightLayer.SKY, HE.blockPosition()) > 10 && level.getSkyDarken() >= 5) {
+                HE.addTag("night");
+            }
+        }
     }
 }
